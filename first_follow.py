a63cbab22d2d1a49a3720fc81d008155d25c1560
pijoules <pijoules@gmail.com>
2017-07-19 15:48:03 -0400

    gotta increase that github score

--- first_follow.py
+++ first_follow.py
@@ -7,11 +7,12 @@ class Parser:
     https://www.cs.virginia.edu/~cs415/reading/FirstFollowLL.pdf
     """
 
-    def __init__(self, rules):
+    def __init__(self, tokens, rules):
         """
         Args:
             rules (list[list[str, list[str]]])
         """
+        self.__tokens = tokens
         self.__rules = rules
         self.__nonterminals = {r[0] for r in rules}
         self.__start_nonterminal = rules[0][0]
@@ -24,6 +25,21 @@ class Parser:
         self.__firsts_dict = {}
         self.__follows_dict = {}
 
+        self.__init_firsts()
+        self.__init_follows()
+
+    def __init_firsts(self):
+        for token in self.__tokens:
+            self.__firsts_dict[token] = self.__find_firsts(token)
+
+        for rule, _ in self.__rules:
+            self.__firsts_dict[rule] = self.__find_firsts(rule)
+
+    def __init_follows(self):
+        # Only rules are in the follows dict
+        for rule, _ in self.__rules:
+            self.__follows_dict[rule] = self.__find_follows(rule)
+
     def firsts_stack(self):
         return self.__firsts_stack
 
@@ -31,10 +47,10 @@ class Parser:
         return self.__follows_stack
 
     def is_terminal(self, symbol):
-        return not self.is_non_terminal(symbol)
+        return symbol in self.__tokens
 
     def is_non_terminal(self, symbol):
-        return symbol in self.__nonterminals
+        return not self.is_terminal(symbol)
 
     def __construct_nonterminal_firsts(self, symbol):
         assert self.is_non_terminal(symbol)
@@ -45,21 +61,24 @@ class Parser:
                 # For each production that mapped to rule X
 
                 # Put firsts(Y1) - {e} into firsts(X)
-                firsts_set |= self.firsts(prod[0]) - {EMPTY}
+                firsts_set |= self.__find_firsts(prod[0]) - {EMPTY}
 
                 # Check for epsilon in each symbol in the prod
                 all_have_empty = True
                 for i in xrange(len(prod) - 1):
-                    if EMPTY in self.firsts(prod[i]):
-                        firsts_set |= self.firsts(prod[i+1]) - {EMPTY}
+                    if EMPTY in self.__find_firsts(prod[i]):
+                        firsts_set |= self.__find_firsts(prod[i+1]) - {EMPTY}
                     else:
                         all_have_empty = False
-                if all_have_empty and EMPTY in self.firsts(prod[-1]):
+                if all_have_empty and EMPTY in self.__find_firsts(prod[-1]):
                     firsts_set.add(EMPTY)
 
         return firsts_set
 
     def firsts(self, symbol):
+        return self.__firsts_dict[symbol]
+
+    def __find_firsts(self, symbol):
         assert isinstance(symbol, basestring)
 
         if symbol in self.__firsts_dict:
@@ -79,6 +98,9 @@ class Parser:
                 return set()
 
     def follows(self, symbol):
+        return self.__follows_dict[symbol]
+
+    def __find_follows(self, symbol):
         assert isinstance(symbol, basestring)
 
         if symbol in self.__follows_dict:
@@ -97,11 +119,11 @@ class Parser:
         for rule, prod in self.__rules:
             for i in xrange(len(prod)-1):
                 if prod[i] == symbol:
-                    follows_set |= self.firsts(prod[i+1]) - {EMPTY}
-                    if EMPTY in self.firsts(prod[i+1]):
-                        follows_set |= self.follows(rule)
+                    follows_set |= self.__find_firsts(prod[i+1]) - {EMPTY}
+                    if EMPTY in self.__find_firsts(prod[i+1]):
+                        follows_set |= self.__find_follows(rule)
             if prod[-1] == symbol:
-                follows_set |= self.follows(rule)
+                follows_set |= self.__find_follows(rule)
 
         self.__follows_dict[symbol] = follows_set
         self.__follows_stack.remove(symbol)
@@ -111,6 +133,7 @@ class Parser:
 
 def test_rules():
     # From the rules_test_sec.txt
+    tokens = {"b", "d", "a", "o", "z", "c"}
     rules = [
         ["S", ["B", "b"]],
         ["S", ["C", "d"]],
@@ -121,7 +144,7 @@ def test_rules():
         ["C", ["c", "C"]],
         ["C", ["o"]],
     ]
-    parser = Parser(rules)
+    parser = Parser(tokens, rules)
 
     # firsts
     assert parser.firsts("S") == {"a", "o", "z", "c"}
@@ -143,6 +166,7 @@ def test_rules():
 def test_rules2():
     # From
     # https://en.wikipedia.org/wiki/Canonical_LR_parser#FIRST_and_FOLLOW_sets
+    tokens = {"(", ")", "n", "+"}
     rules = [
         ["S", ["E"]],
         ["E", ["T"]],
@@ -151,7 +175,7 @@ def test_rules2():
         ["T", ["+", "T"]],
         ["T", ["T", "+", "T"]],
     ]
-    parser = Parser(rules)
+    parser = Parser(tokens, rules)
 
     # firsts
     assert parser.firsts("S") == {"n", "+", "("}
@@ -171,6 +195,7 @@ def test_rules2():
 def test_rules3():
     # From
     # http://www.montefiore.ulg.ac.be/~geurts/Cours/compil/2012/03-syntaxanalysis-2-2012-2013.pdf
+    tokens = {"+", "*", "(", ")", "id"}
     rules = [
         ["E", ["E", "+", "T"]],
         ["E", ["T"]],
@@ -179,7 +204,7 @@ def test_rules3():
         ["F", ["(", "E", ")"]],
         ["F", ["id"]],
     ]
-    parser = Parser(rules)
+    parser = Parser(tokens, rules)
 
     # firsts
     assert parser.firsts("E") == parser.firsts("T") == parser.firsts("F") == {"id", "("}
@@ -195,6 +220,7 @@ def test_rules3():
 
 def test_rules4():
     # My test rules
+    tokens = {"ADD", "SUB", "MUL", "DIV", "NAME", "INT"}
     rules = [
         ["module", ["expr"]],
         ["expr", ["expr", "ADD", "expr"]],
@@ -204,7 +230,7 @@ def test_rules4():
         ["expr", ["NAME"]],
         ["expr", ["INT"]],
     ]
-    parser = Parser(rules)
+    parser = Parser(tokens, rules)
 
     # firsts
     assert parser.firsts("expr") == parser.firsts("module") == {"NAME", "INT"}
@@ -220,12 +246,13 @@ def test_rules4():
 
 def test_rules5():
     # Using epsilon
+    tokens = {"a"}
     rules = [
         ["S", ["X"]],
         ["X", ["a"]],
         ["X", [EMPTY]],
     ]
-    parser = Parser(rules)
+    parser = Parser(tokens, rules)
 
     # firsts
     assert parser.firsts("X") == parser.firsts("S") == {"a", EMPTY}
